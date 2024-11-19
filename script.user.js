b85c50dcee016420943f7bae729862da49e7c7a0
Amukerd <146035837+amukerd@users.noreply.github.com>
2024-11-19 17:56:21 -0500

    SnapFocus v1

---
+++ script.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         SnapFocus
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @updateURL    
+// @downloadURL  
+// @description  Stop the ui cover from showing up when the tab is unfocused, by stopping the tab from knowing it's unfocused
+// @author       Amukerd
+// @match        https://web.snapchat.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    const preventFocusLoss = (event) => {
+        if (event.type === 'visibilitychange' && document.visibilityState !== 'hidden') {
+            return;
+        }
+        event.preventDefault();
+        event.stopImmediatePropagation();
+    };
+
+    document.addEventListener('visibilitychange', preventFocusLoss);
+    window.addEventListener('focus', preventFocusLoss);
+    window.addEventListener('blur', preventFocusLoss);
+    window.addEventListener('focusin', preventFocusLoss);
+    window.addEventListener('focusout', preventFocusLoss);
+    window.addEventListener('resize', preventFocusLoss);
+})();
